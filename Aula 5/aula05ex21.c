133e506c50198efece81f8be95fea3bf4cc533d6
Ângelo Garcia <fhzkngj@gmail.com>
2023-04-26 14:23:23 -0400

    Add solution to aula05ex21.c

--- Aula 5/aula05ex21.c
+++ Aula 5/aula05ex21.c
@@ -1,3 +1,35 @@
 // Aluno: Ângelo Garcia Fernandez
 // Matrícula: 2022010034
 // Disciplina: Programação Estruturada
+/* 21. Escreva um programa que leia certa quantidade de números, imprima o maior deles e quantas
+vezes o maior número foi lido. A quantidade de números a serem lidos deve ser fornecida pelo
+usuário.*/
+#include <stdio.h>
+
+int main() {
+    int number, maxNumber, maxCount, iterations, count;
+
+    printf("Enter how much numbers do you wanna enter: ");
+    scanf("%d", &iterations);
+
+    for (count = 0; count < iterations; count++) {
+        printf("Enter an integer: ");
+        scanf("%d", &number);
+
+        if (count == 0) {
+            maxNumber = number;
+            maxCount++;
+        } else if (number > maxNumber) {
+            maxCount = 0;
+            maxNumber = number;
+            maxCount++;
+        } else if (number == maxNumber) {
+            maxCount++;
+        }
+    }
+
+    printf("Max number entered: %d\n", maxNumber);
+    printf("Times entered: %d\n", maxCount);
+
+    return 0;
+}
