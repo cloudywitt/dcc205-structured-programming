bdf7dc834a936720ef4d1a7999c07d237afd4548
Ângelo Garcia <fhzkngj@gmail.com>
2023-05-24 10:01:57 -0400

    Add solution to aula08ex08.c

---
+++ Aula 8/aula08ex08.c
@@ -0,0 +1,35 @@
+// Aluno: Ângelo Garcia Fernandez
+// Matrícula: 2022010034
+// Disciplina: Programação Estruturada
+/* 8. Elabore uma função para veriﬁcar se um número é um quadrado perfeito. Um quadrado
+perfeito é um número inteiro não negativo que pode ser expresso como o quadrado de
+outro número inteiro. Exemplos: 1, 4, 9.*/
+#include <stdio.h>
+#include <math.h>
+
+int isSquare(int num) {
+    int isSquare;
+
+    if (sqrt(num) == (int) sqrt(num)) {
+        isSquare = 1;
+    } else {
+        isSquare = 0;
+    }
+
+    return isSquare;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isSquare(num)) {
+        printf("The number is a square.\n");
+    } else {
+        printf("The number is not a square.\n");
+    }
+
+    return 0;
+}
