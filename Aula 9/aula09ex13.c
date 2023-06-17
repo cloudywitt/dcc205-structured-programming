aadc72865ef75a86b7bdce4d23cd0d18e4fe13a8
Ângelo Garcia <fhzkngj@gmail.com>
2023-06-17 12:37:50 -0400

    Add solution to aula09ex13.c

---
+++ Aula 9/aula09ex13.c
@@ -0,0 +1,50 @@
+// Aluno: Ângelo Garcia Fernandez
+// Matrícula: 2022010034
+// Disciplina: Programação Estruturada
+/* 13. Faça um programa que leia um arquivo texto contendo os dados de um aluno. Cada
+linha do arquivo contém o número de matrícula, o nome e as notas de três provas de
+um aluno. Os dados devem ser lidos e armazenados em uma estrutura. Em seguida,
+exiba os dados lidos em tela.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE* openFile(char *fname, const char * restrict mode) {
+    FILE* file = fopen(fname, mode);
+
+    if (file == NULL) {
+        printf("Couldn't open %s\n", fname);
+
+        exit(1);
+    }
+
+    return file;
+}
+
+typedef struct {
+    unsigned int id;
+    char name[30];
+    float scores[3];
+} Student;
+
+int main() {
+    FILE* studentsPtr = openFile("students-registration.txt", "r");
+
+    Student students[4];
+
+    int i = 0;
+
+    while (fscanf(studentsPtr, "%u %s %f %f %f", &students[i].id, students[i].name, &students[i].scores[0], &students[i].scores[1], &students[i].scores[2]) != EOF) {
+        i++;
+    }
+
+    printf("--------------------\n");
+    for (int i = 0; i < 4; i++) {
+        printf("ID: %u\n", students[i].id);
+        printf("Name: %s\n", students[i].name);
+        printf("Scores: %.1f %.1f %.1f\n", students[i].scores[0], students[i].scores[1], students[i].scores[2]);
+        printf("--------------------\n");
+
+    }
+
+    return 0;
+}
